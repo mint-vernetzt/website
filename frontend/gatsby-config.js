e11a8d700d353f1fe509d168c5980bf61694d94e
Peter Holló <hello@songsforthe.dev>
2022-03-16 12:54:22 +0100

    intialize gatsby

---
+++ frontend/gatsby-config.js
@@ -0,0 +1,15 @@
+/** @type {import('gatsby').GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+    title: ``,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        url: "https://stage.cms.mint-vernetzt.de/wp/graphql",
+      },
+    },
+  ],
+};
