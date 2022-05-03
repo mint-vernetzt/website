1fe9cbe86f67f58a7eb18e97cb4bff461699517f
Peter Holló <hello@songsforthe.dev>
2022-05-03 16:28:31 +0200

    add page

---
+++ frontend/src/pages/mintraketen.tsx
@@ -0,0 +1,21 @@
+import { H1 } from "../components/Heading/Heading";
+import Layout from "../components/Layout";
+import SEO from "../components/SEO";
+
+export function Mintraketen() {
+  return (
+    <Layout>
+      <SEO
+        title="MINTraketen"
+        slug="/mintraketen"
+        description=""
+        image=""
+        children=""
+      />
+
+      <H1>MINTraketen</H1>
+    </Layout>
+  );
+}
+
+export default Mintraketen;
