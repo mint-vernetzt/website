f3742faefd98f0b8e6f21ede4de81e83ccf8a787
nac62116 <cpnash@protonmail.ch>
2022-05-04 12:30:39 +0200

    Added "Wissenschaftliche Forschungserkenntnisse" teaserbox

--- frontend/src/pages/mintraketen.tsx
+++ frontend/src/pages/mintraketen.tsx
@@ -36,11 +36,12 @@ export function Mintraketen() {
         children=""
       />
 
+      {/* TODO: Text style and spacing is not equal to the MINTraketen.pdf template in this <section> */}
       <section className="container my-8 md:my-10 lg:my-20">
         <div className="hero hero-news flex items-end rounded-3xl relative overflow-hidden">
           {/*data?.HeroImage?.childImageSharp?.gatsbyImageData !== undefined ? (
             Question: Do we need this condition?
-            null assignment leads to runtime error
+            null assignment leads to runtime error,
             an undefined image property of the GatsbyImage does not (gray image instead)*/
             <GatsbyImage
               image={undefined /* TODO */}
@@ -48,7 +49,6 @@ export function Mintraketen() {
               alt="Konkrete Gute-Praxis-Beispiele von MINT- Bildungsanbieter:innen."
             />
           /*) : null*/}
-          {/* TODO: Text style is not equal to the MINTraketen.pdf template */}
           <div className="hero-text absolute top-0 left-0 h-full right-0 pt-12 px-4 md:px-12 md:flex md:items-center lg:px-20">
             <div className="md:flex-100">
               <H1 like="h0"> {/* Question: Why can't we use H0 here instead of H1 like="h0" */}
@@ -62,9 +62,10 @@ export function Mintraketen() {
         </div>
       </section>
 
+      {/* TODO: Text style and spacing is not equal to the MINTraketen.pdf template in this <section> */}
       <section className="container mt-8 md:mb-10 lg:mt-10 mb-8 md:mb-10 lg:mb-20">
         <header>
-          <p className="text-base md:px-8 lg:px-20 ">{/* TODO: Text style is not equal to the MINTraketen.pdf template */}
+          <p className="text-base md:px-8 lg:px-20 ">
           Die MINTrakete ist eine Auszeichnung für außergewöhnliche Gute-Praxis-Beispiele 
           in der MINT-Bildung. Mithilfe regelmäßiger Ausschreibungen rufen wir zu wechselnden 
           Themen zur Bewerbung auf und bieten so verschiedenen Bildungsangeboten eine Bühne. 
@@ -77,6 +78,7 @@ export function Mintraketen() {
         </header>
       </section>
 
+      {/* TODO: Text style and spacing is not equal to the MINTraketen.pdf template in this <section> */}
       <section className="container mt-8 md:mb-10 lg:mt-10 mb-8 md:mb-10 lg:mb-20">
         <div className="grid gap-4 lg:gap-8 grid-cols-1 md:grid-cols-2">
           {[
@@ -113,6 +115,7 @@ export function Mintraketen() {
         </div>
       </section>
 
+      {/* TODO: Text style and spacing is not equal to the MINTraketen.pdf template in this <section> */}
       <section className="container my-8 md:my-10 lg:my-20">
         <div className="flex flex-wrap content-center items-center md:-mx-6 lg:-mx-10">
           <div className="flex-100 pb-6 md:pb-0 md:flex-1/3 lg:flex-1/4 md:px-6 lg:px-10">
@@ -123,7 +126,6 @@ export function Mintraketen() {
               alt="MINTrakete"
             /> */}
           </div>
-          {/* TODO: Text style is not equal to the MINTraketen.pdf template */}
           <div className="flex-100 md:flex-2/3 md:px-6 lg:px-10">
             <p className="lg:text-3xl mb-2 lg:mb-4 font-bold">
               Lerne hier unsere MINTraketen näher kennen
@@ -139,6 +141,7 @@ export function Mintraketen() {
         </div>
       </section>
 
+      {/* TODO: Text style and spacing is not equal to the MINTraketen.pdf template in this <section> */}
       <section className="container mt-8 md:mb-10 lg:mt-10 mb-8 md:mb-10 lg:mb-20">
         <div className="grid gap-4 lg:gap-8 grid-cols-3 md:grid-cols-3">
           { // TODO: Build array with cms data
@@ -163,24 +166,22 @@ export function Mintraketen() {
                     alt=""
                   />
                 </div>
-                {/* TODO: Text style is not equal to the MINTraketen.pdf template */}
                 <H4 className="lg:leading-snug lg:mx-2">{teaserbox.title}</H4>
                 <p className="lg:leading-snug lg:mx-2">{teaserbox.subtitle}</p>
                 <p className="lg:mx-2">{teaserbox.text}</p>
               </Link>
-              {/* TODO: Text style is not equal to the MINTraketen.pdf template */}
+              {/* TODO: Styling the link element */}
               <a href={teaserbox.externalLink} className="lg:leading-snug lg:mx-2">{teaserbox.externalLink}</a>
             </div>
           ))}
         </div>
       </section>
 
+      {/* TODO: Text style and spacing is not equal to the MINTraketen.pdf template in this <section> */}
       <section className="container mt-8 md:mb-10 lg:mt-10 mb-8 md:mb-10 lg:mb-20">
-        {/* TODO: Text style is not equal to the MINTraketen.pdf template */}
         <H2 like="h1">
           <strong>MINT</strong>news
         </H2>
-        {/* TODO: Spacing between header and grid is not equal to the MINTraketen.pdf template */}
         <div className="grid gap-4 lg:gap-8 grid-cols-1 md:grid-cols-2">
           { // TODO: Get data from cms
           [
@@ -229,7 +230,6 @@ export function Mintraketen() {
                     alt=""
                   />
                 </div>
-                {/* TODO: Text style is not equal to the MINTraketen.pdf template */}
                 <H4 className="lg:leading-snug lg:mx-2">{teaserbox.title}</H4>
                 <p className="lg:mx-2">{teaserbox.text}</p>
                 <ul className="lg:mx-2 flex flex-wrap md:order-4 z-10">
@@ -242,6 +242,45 @@ export function Mintraketen() {
                     );
                   })}
                 </ul>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {/* TODO: Text style and spacing is not equal to the MINTraketen.pdf template in this <section> */}
+      <section className="container mt-8 md:mb-10 lg:mt-10 mb-8 md:mb-10 lg:mb-20">
+        <H2>
+          Wissenschaftliche Forschungserkenntnisse
+        </H2>
+        <p>
+          Ihr habt Interesse an theoretischen Erkenntnissen zu Gute-Praxis-Beispielen?<br></br>
+          Dann findet Ihr auf unserer Seite MINT-Bildungsforschung weitere spannende Einblicke.
+        </p>
+        <div className="grid gap-4 lg:gap-8 grid-cols-1 md:grid-cols-2">
+          {
+          [
+            {
+              image: undefined,
+              title: `Gendersensible MINT-Didaktik`,
+              text: `Erfahrt hier, mit welchen didaktischen Maßnahmen man Mädchen für einzelne MINT- Disziplinen begeistern kann und wie wichtige Formate der Begabtenförderung immer noch von Stereotypen beeinflusst werden.`,
+              link: `/mint-bildungsforschung/`,
+            },
+          ].map((teaserbox, index) => (
+            <div
+              key={`teaserbox-${index}`}
+              className="p-4 pb-8 md:p-2 md:pb-8 lg:p-4 lg:pb-8 rounded-lg bg-neutral-200 shadow-lg"
+            >
+              <Link to={`${teaserbox.link}`} className="flex flex-col h-100">
+                <div className="rounded-lg overflow-hidden mb-2 lg:mb-4">
+                  <GatsbyImage
+                    image={teaserbox.image}
+                    className="w-full h-auto"
+                    alt=""
+                  />
+                </div>
+                <H4 className="lg:leading-snug lg:mx-2">{teaserbox.title}</H4>
+                <p className="lg:mx-2">{teaserbox.text}</p>
               </Link>
             </div>
           ))}
