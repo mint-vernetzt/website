581d4fbb1eef896dd7ead191c602729302a87ef9
nac62116 <56918308+nac62116@users.noreply.github.com>
2022-05-05 14:58:42 +0200

    Added last TODO's

--- frontend/src/pages/mintraketen.tsx
+++ frontend/src/pages/mintraketen.tsx
@@ -40,7 +40,6 @@ export function Mintraketen({ data }) {
         </div>
       </section>
 
-      {/* TODO: Text style and spacing is not equal to the MINTraketen.pdf template in this <section> */}
       <section className="container mt-8 md:mb-10 lg:mt-10 mb-8 md:mb-10 lg:mb-20">
         <header>
           <p className="text-xl md:px-8 lg:px-20 ">
@@ -128,68 +127,67 @@ export function Mintraketen({ data }) {
         </div>
       </section>
 
-      {/* TODO: Text style and spacing is not equal to the MINTraketen.pdf template in this <section> */}
       <section className="container mt-8 md:mb-10 lg:mt-10 mb-8 md:mb-10 lg:mb-20">
-        {/* The number of rows in the grid are set automatically, which makes the class "grid-cols-3" obsolete in this context. */}
-        <div className="grid gap-4 lg:gap-8 md:grid-cols-3">
-          {data.projects.nodes.map((project, index) => (
-            <div
-              key={`teaserbox-${index}`}
-              className="p-4 pb-8 md:p-2 md:pb-8 lg:p-4 lg:pb-8 rounded-lg bg-neutral-200 shadow-lg"
-            >
-              {/* /project/ does not exist in this environment */}
-              <Link
-                to={`/project/${project.slug}`}
-                className="flex flex-col h-100"
+        <div className="grid gap-4 lg:gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {data.projects.nodes.map((project, index) => {
+            return (
+              <div
+                key={`teaserbox-${index}`}
+                className="p-4 pb-8 md:p-2 md:pb-8 lg:p-4 lg:pb-8 rounded-lg bg-neutral-200 shadow-lg"
               >
-                {/* TODO: Positioning the subimage */}
-                <div className="overflow-hidden mb-2 lg:mb-4">
-                  <GatsbyImage
-                    image={
-                      project.projectInformations.projectLogo.localFile
-                        .childImageSharp.gatsbyImageData
-                    }
-                    className="w-20 h-auto"
-                    alt=""
-                  />
-                  <H3 like="h5" className="lg:leading-snug lg:mx-2">
-                    {project.title}
-                  </H3>
-                  <p className="lg:leading-snug lg:mx-2 text-xs">
-                    <strong>{project.projectInformations.institution}</strong>
+                <Link
+                  to={`/project/${project.slug}`}
+                  className="flex flex-col h-100"
+                >
+                  {/* TODO: Positioning the subimage */}
+                  <div className="overflow-hidden mb-2 lg:mb-4">
+                    <GatsbyImage
+                      image={
+                        project.projectInformations.projectLogo.localFile
+                          .childImageSharp.gatsbyImageData
+                      }
+                      className="w-20 h-auto"
+                      alt=""
+                    />
+                    <H3 like="h5" className="lg:leading-snug lg:mx-2">
+                      {project.title}
+                    </H3>
+                    <p className="lg:leading-snug lg:mx-2 text-xs">
+                      <strong>{project.projectInformations.institution}</strong>
+                    </p>
+                  </div>
+                  <p className="line-clamp-2 lg:mx-2">
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: project.excerpt as string,
+                      }}
+                    />
                   </p>
-                </div>
-                <p className="line-clamp-2 lg:mx-2">
-                  <span
-                    dangerouslySetInnerHTML={{
-                      __html: project.excerpt as string,
-                    }}
-                  />
-                </p>
-              </Link>
-              {/* TODO: Styling the link element */}
-              <a
-                href={project.projectInformations.projectWebsite}
-                className="lg:leading-snug lg:mx-2 text-lilac-500 font-bold hover:underline"
-              >
-                {/* TODO: Put this inside a function "getShortenedUrl()" and add "www." if !string.includes("www.")
+                </Link>
+                {/* TODO: Styling the link element (margin) */}
+                <a
+                  href={project.projectInformations.projectWebsite}
+                  className="lg:leading-snug lg:mx-2 text-lilac-500 font-bold hover:underline"
+                >
+                  {/* 
+                    TODO: Put this inside a function "getShortenedUrl()" and add "www." if !string.includes("www.")
                     shortenedUrl === null ? project.projectInformations.projectWebsite : shortenedUrl
-                    Do i have to do this before the return statement by iterating throug the data? */}
-                {project.projectInformations.projectWebsite
-                  .match(/\/\/[^\/]+\//g)[0]
-                  .substring(
-                    2,
-                    project.projectInformations.projectWebsite.match(
-                      /\/\/[^\/]+\//g
-                    )[0].length - 1
-                  )}
-              </a>
-            </div>
-          ))}
+                    */}
+                  {project.projectInformations.projectWebsite
+                    .match(/\/\/[^\/]+\//g)[0]
+                    .substring(
+                      2,
+                      project.projectInformations.projectWebsite.match(
+                        /\/\/[^\/]+\//g
+                      )[0].length - 1
+                    )}
+                </a>
+              </div>
+            );
+          })}
         </div>
       </section>
 
-      {/* TODO: Text style and spacing is not equal to the MINTraketen.pdf template in this <section> */}
       <section className="container mt-8 md:mb-10 lg:mt-10 mb-8 md:mb-10 lg:mb-20">
         <H2 like="h1">
           <strong>MINT</strong>news
@@ -240,7 +238,6 @@ export function Mintraketen({ data }) {
         </div>
       </section>
 
-      {/* TODO: Text style and spacing is not equal to the MINTraketen.pdf template in this <section> */}
       <section className="container mt-8 md:mb-10 lg:mt-10 mb-8 md:mb-10 lg:mb-20">
         <H2>Wissenschaftliche Forschungserkenntnisse</H2>
         <p>
