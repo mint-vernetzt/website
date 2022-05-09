60a95c85e808c4cf2a94ad517d9cb1dafae6d1af
Peter Holló <hello@songsforthe.dev>
2022-05-09 10:11:47 +0200

    feat: overview page mint raketen (#14)
    * add page
    * Added top section with gatsby image component
    * Added "Aktuelle/Vergangene Ausschreibungen"
    * Added BadgeRocket with side text
    * Added static mintraketen teaserboxes
    * Added static MINTnews teaserboxes
    * Added "Wissenschaftliche Forschungserkenntnisse"
    teaserbox
    * Removed array mapping from
    "Wissenschaftliche Erkenntnisse" teaserbox
    * Changed external link style
    * Inserted graphql page query
    * Inserted cms data
    * Styled BadgeRocket section
    * Changed resposiveness of BadgeRockets
    * Added getHostnameFromURL() to dataTransformer.tsx
    * hero image update
    * order news and projects
    * smaller font on mobile
    * more news columns on large screens
    
    Co-authored-by: nac62116 <56918308+nac62116@users.noreply.github.com>

--- frontend/src/pages/projects.tsx
+++ frontend/src/pages/projects.tsx
@@ -60,7 +60,7 @@ export function Projects({ data }) {
 
       <section className="container my-8 md:my-10 lg:my-20">
         <div className="flex flex-wrap content-center items-center md:-mx-6 lg:-mx-10">
-          <div className="flex-100 pb-6 md:pb-0 md:flex-1/3 lg:flex-1/4 md:px-6 lg:px-10">
+          <div className="flex-1/4 pb-6 md:pb-0 md:flex-1/3 lg:flex-1/4 md:px-6 lg:px-10">
             <BadgeRocket />
             {/* <img
               className="w-full h-auto"
