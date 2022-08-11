dc56e2277c772a05010ef330c82f57b3aa77b5c8
nac62116 <56918308+nac62116@users.noreply.github.com>
2022-08-11 11:03:28 +0200

    make privacy.tsx type safe

--- frontend/src/pages/privacy.tsx
+++ frontend/src/pages/privacy.tsx
@@ -6,21 +6,21 @@ import { H1 } from "../components/Heading/Heading";
 /* eslint-disable-next-line */
 export interface PrivacyProps {}
 
-export function Privacy({ data }) {
+export function Privacy({ data }: { data: GatsbyTypes.PrivacyPageQuery }) {
   return (
     <Layout>
       <SEO
-        title={`${data.privacyPage.title}`}
-        slug={`${data.privacyPage.slug}`}
+        title={data.privacyPage?.title || ""}
+        slug={data.privacyPage?.slug || ""}
         children={""}
         image=""
         description=""
       />
       <section className="hero container my-8 md:my-10 lg:my-20">
-        <H1>{data.privacyPage.title}</H1>
+        <H1>{data.privacyPage?.title || ""}</H1>
         <div
           className="page-content"
-          dangerouslySetInnerHTML={{ __html: data.privacyPage.content }}
+          dangerouslySetInnerHTML={{ __html: data.privacyPage?.content || "" }}
         />
       </section>
     </Layout>
