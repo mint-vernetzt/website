ad42f3c49dd71cb62f031242a9754571a3608210
nac62116 <56918308+nac62116@users.noreply.github.com>
2022-08-11 11:16:12 +0200

    Make imprint.tsx type safe

--- frontend/src/pages/imprint.tsx
+++ frontend/src/pages/imprint.tsx
@@ -1,21 +1,25 @@
+import { graphql } from "gatsby";
 import { H1, H2, H3 } from "../components/Heading/Heading";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 
-/* eslint-disable-next-line */
-export interface ImprintProps {}
-
-export function Imprint(props: ImprintProps) {
+export function Imprint({ data }: { data: GatsbyTypes.ImprintPageQuery }) {
   return (
     <Layout>
       <SEO
-        title="Impressum"
+        title={data.imprintPage?.title || ""}
+        slug={data.imprintPage?.slug || ""}
         image=""
         description="Impressum"
-        slug="/imprint/"
         children={""}
       />
       <section className="container my-8 md:my-10 lg:my-20">
+        <H1>{data.imprintPage?.title || ""}</H1>
+        <div
+          className="page-content"
+          dangerouslySetInnerHTML={{ __html: data.imprintPage?.content || "" }}
+        />
+
         <H1>Impressum</H1>
         <p>
           Körber-Stiftung
@@ -237,5 +241,15 @@ export function Imprint(props: ImprintProps) {
     </Layout>
   );
 }
+
+export const imprintPageQuery = graphql`
+  query ImprintPage {
+    imprintPage: wpPage(slug: { eq: "imprint" }) {
+      content
+      title
+      slug
+    }
+  }
+`;
 
 export default Imprint;
