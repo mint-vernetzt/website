1c2903239c4afddf8aba521a0694f98b82359926
Peter Holló <hello@songsforthe.dev>
2022-10-25 17:19:22 +0200

    add newsletter and stuff page

---
+++ frontend/src/pages/mintvernetzt-informiert.tsx
@@ -0,0 +1,7 @@
+import { H1 } from "../components/Heading/Heading";
+
+export function MintVernetztInformiert() {
+  return <H1 like="h0">MINTvernetzt informiert</H1>;
+}
+
+export default MintVernetztInformiert;
