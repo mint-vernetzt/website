8859982113eb36dd98b1e27729f4700cb9740d0e
nac62116 <56918308+nac62116@users.noreply.github.com>
2022-10-26 16:52:21 +0200

    Only show events/news when they exist

--- frontend/src/pages/topics/gender-sensible-mint-bildung.tsx
+++ frontend/src/pages/topics/gender-sensible-mint-bildung.tsx
@@ -69,44 +69,46 @@ export function GenderSensibleMintBildung({
           </div>
         </header>
       </section>
-
-      <section className="container mt-8 md:mb-10 lg:mt-10 mb-8 md:mb-10 lg:mb-20">
-        <header>
-          <H2 like="h1">Unsere Blogbeiträge zum Thema Gender</H2>
-          <p className="text-xl md:px-8 lg:px-20 ">
-            In unserer Auswahl von Blogbeiträgen findet Ihr konkrete Tipps,
-            wissenschaftliche Hintergründe und spannende Projekte zum Reinlesen.
+      {data.news.nodes.length > 0 && (
+        <section className="container mt-8 md:mb-10 lg:mt-10 mb-8 md:mb-10 lg:mb-20">
+          <header>
+            <H2 like="h1">Unsere Blogbeiträge zum Thema Gender</H2>
+            <p className="text-xl md:px-8 lg:px-20 ">
+              In unserer Auswahl von Blogbeiträgen findet Ihr konkrete Tipps,
+              wissenschaftliche Hintergründe und spannende Projekte zum
+              Reinlesen.
+            </p>
+          </header>
+          <div className="grid gap-4 lg:gap-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+            {data.news.nodes.map((news, index) => (
+              <div
+                key={`news-${index}`}
+                className="p-4 rounded-lg bg-neutral-200 shadow-lg"
+              >
+                <Link to={`/news/${news.slug}`} className="flex flex-col h-100">
+                  <H4 className="lg:leading-snug lg:mx-2">{news.title}</H4>
+                  <div
+                    className="line-clamp-3 lg:mx-2"
+                    dangerouslySetInnerHTML={{
+                      __html: news.excerpt as string,
+                    }}
+                  />
+                </Link>
+              </div>
+            ))}
+          </div>
+          <p className="md:text-center">
+            <Link to="/news/?tags=gender" className="btn-primary mt-8">
+              Zur Beitragsübersicht
+            </Link>
           </p>
-        </header>
-        <div className="grid gap-4 lg:gap-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-          {data.news.nodes.map((news, index) => (
-            <div
-              key={`news-${index}`}
-              className="p-4 rounded-lg bg-neutral-200 shadow-lg"
-            >
-              <Link to={`/news/${news.slug}`} className="flex flex-col h-100">
-                <H4 className="lg:leading-snug lg:mx-2">{news.title}</H4>
-                <div
-                  className="line-clamp-3 lg:mx-2"
-                  dangerouslySetInnerHTML={{
-                    __html: news.excerpt as string,
-                  }}
-                />
-              </Link>
-            </div>
-          ))}
-        </div>
-        <p>
-          <Link to="news/?tags=gender" className="btn-primary">
-            Zur Beitragsübersicht
-          </Link>
-        </p>
-      </section>
+        </section>
+      )}
 
       <section className="container mt-8 md:mb-10 lg:mt-10 mb-8 md:mb-10 lg:mb-20">
-        <div className="flex flex-wrap content-center items-stretch md:-mx-4">
+        <div className="flex flex-wrap content-center items-center md:-mx-4">
           <div className="w-full md:w-1/2 flex-initial mb-6 md:mb-0 md:px-4">
-            <div className="h-full bg-[#EFE8E6] py-24 flex items-center rounded-lg">
+            <div className="rounded-lg overflow-hidden">
               <GatsbyImage
                 image={
                   data.bildungsforschungImage?.childImageSharp?.gatsbyImageData
@@ -143,44 +145,49 @@ export function GenderSensibleMintBildung({
         </div>
       </section>
 
-      <section className="container mt-8 md:mb-10 lg:mt-10 mb-8 md:mb-10 lg:mb-20">
-        <header>
-          <H2 like="h1">Unsere Events zum Thema Gender</H2>
-          <p className="text-xl md:px-8 lg:px-20 ">
-            Kommt mit uns in den Austausch, informiert und vernetzt Euch mit
-            anderen MINT-Akteur:innen in unseren Veranstaltungen zum Thema
-            Frauen und Mädchen in MINT.
+      {data.events.nodes.length > 0 && (
+        <section className="container mt-8 md:mb-10 lg:mt-10 mb-8 md:mb-10 lg:mb-20">
+          <header>
+            <H2 like="h1">Unsere Events zum Thema Gender</H2>
+            <p className="text-xl md:px-8 lg:px-20 ">
+              Kommt mit uns in den Austausch, informiert und vernetzt Euch mit
+              anderen MINT-Akteur:innen in unseren Veranstaltungen zum Thema
+              Frauen und Mädchen in MINT.
+            </p>
+          </header>
+          <div className="grid gap-4 lg:gap-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+            {data.events.nodes.map((event, index) => (
+              <div
+                key={`event-${index}`}
+                className="p-4 rounded-lg bg-neutral-200 shadow-lg"
+              >
+                <Link
+                  to={`/news/${event.slug}`}
+                  className="flex flex-col h-100"
+                >
+                  <H4 className="lg:leading-snug lg:mx-2">{event.title}</H4>
+                  <div
+                    className="line-clamp-3 lg:mx-2"
+                    dangerouslySetInnerHTML={{
+                      __html: event.excerpt as string,
+                    }}
+                  />
+                </Link>
+              </div>
+            ))}
+          </div>
+          <p className="md:text-center">
+            <Link to="/events/?tags=gender" className="btn-primary mt-8">
+              Zur Eventübersicht
+            </Link>
           </p>
-        </header>
-        <div className="grid gap-4 lg:gap-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-          {data.events.nodes.map((event, index) => (
-            <div
-              key={`event-${index}`}
-              className="p-4 rounded-lg bg-neutral-200 shadow-lg"
-            >
-              <Link to={`/news/${event.slug}`} className="flex flex-col h-100">
-                <H4 className="lg:leading-snug lg:mx-2">{event.title}</H4>
-                <div
-                  className="line-clamp-3 lg:mx-2"
-                  dangerouslySetInnerHTML={{
-                    __html: event.excerpt as string,
-                  }}
-                />
-              </Link>
-            </div>
-          ))}
-        </div>
-        <p>
-          <Link to="events/?tags=gender" className="btn-primary">
-            Zur Eventübersicht
-          </Link>
-        </p>
-      </section>
+        </section>
+      )}
 
       <section className="container mt-8 md:mb-10 lg:mt-10 mb-8 md:mb-10 lg:mb-20">
-        <div className="flex flex-wrap content-center items-stretch md:-mx-4">
+        <div className="flex flex-wrap content-center items-center md:-mx-4">
           <div className="w-full md:w-1/2 flex-initial mb-6 md:mb-0 md:px-4">
-            <div className="h-full bg-[#EFE8E6] py-24 flex items-center rounded-lg">
+            <div className="overflow-hidden rounded-lg">
               <GatsbyImage
                 image={data.paktImage?.childImageSharp?.gatsbyImageData}
                 className="w-full h-full"
@@ -209,7 +216,7 @@ export function GenderSensibleMintBildung({
                 Deutschland zu sichern.
               </p>
               <p>
-                <Link to="/pakt/" className="btn-primary">
+                <Link to="/projects/mint-pakt" className="btn-primary">
                   Mehr erfahren
                 </Link>
               </p>
@@ -235,7 +242,7 @@ export function GenderSensibleMintBildung({
       </section>
 
       <section className="container mt-8 md:mb-10 lg:mt-10 mb-8 md:mb-10 lg:mb-20">
-        <div className="grid gap-4 lg:gap-8 grid-cols-1 md:grid-cols-3">
+        <div className="grid gap-4 lg:gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {[
             {
               image: data?.befragungImage?.childImageSharp?.gatsbyImageData,
@@ -272,65 +279,6 @@ export function GenderSensibleMintBildung({
           ))}
         </div>
       </section>
-
-      {/* <section className="container mt-8 md:mb-10 lg:mt-10 mb-8 md:mb-10 lg:mb-20">
-        <div className="grid gap-4 lg:gap-8 grid-cols-1 md:grid-cols-2">
-          {[
-            {
-              image:
-                data?.TellerrandArtikelImage?.childImageSharp?.gatsbyImageData,
-              title: `MINT+ oder warum wir den Blick über den Tellerrand wagen sollten`,
-              text: `Was ist MINT+ und inwieweit kann das „plus“ – die Erweiterung von MINT – hilfreich für Euch und Eure Projekte sein? Mit diesem Blogbeitrag bekommt Ihr einen ersten Einblick in das Thema - viel Spaß beim Lesen!`,
-              link: `/news/mint-oder-warum-wir-den-blick-uber-den-tellerrand-wagen-sollten/`,
-            },
-          ].map((teaserbox, index) => (
-            <div
-              key={`teaserbox-${index}`}
-              className="p-4 pb-8 md:p-2 md:pb-8 lg:p-4 lg:pb-8 rounded-lg bg-neutral-200 shadow-lg"
-            >
-              <Link to={`${teaserbox.link}`} className="flex flex-col h-100">
-                <div className="rounded-lg overflow-hidden mb-2 lg:mb-4">
-                  <GatsbyImage
-                    image={teaserbox.image}
-                    className="w-full h-auto"
-                    alt={teaserbox.title}
-                  />
-                </div>
-                <H2 like="h4" className="lg:leading-snug lg:mx-2">
-                  {teaserbox.title}
-                </H2>
-                <p className="lg:mx-2">{teaserbox.text}</p>
-                <p>
-                  <button className="btn-primary">Jetzt lesen</button>
-                </p>
-              </Link>
-            </div>
-          ))}
-          <div className="p-4 pb-8 md:p-2 md:pb-8 lg:p-4 lg:pb-8 rounded-lg bg-neutral-200 shadow-lg flex flex-col h-100">
-            <div className="rounded-lg overflow-hidden mb-2 lg:mb-4">
-              <GatsbyImage
-                image={data.LernenArtikelImage.childImageSharp.gatsbyImageData}
-                className="w-full h-auto"
-                alt="Lernen im Kontext – ein wichtiger Baustein für MINT+"
-              />
-            </div>
-            <H2 like="h4" className="lg:leading-snug lg:mx-2">
-              Lernen im Kontext – ein wichtiger Baustein für MINT+
-            </H2>
-            <p className="lg:mx-2">
-              Was gibt es für Konzepte von „Kontextualisiertem Lernen“, welche
-              Methoden wurden von der Forschung begleitet und als wirksam
-              erkannt und wo gibt es trotzdem noch Herausforderungen in der
-              schulischen und außerschulischen Praxis? Ein Überblick aus der
-              Bildungsforschung.
-            </p>
-          </div>
-        </div>
-      </section>
-
-      
-
-       */}
     </Layout>
   );
 }
