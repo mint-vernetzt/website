f19dcdfc48ab6e97ebc1bdb2df2dc32ba185552a
nac62116 <56918308+nac62116@users.noreply.github.com>
2023-04-26 16:13:57 +0200

    fix links to news and events on page diversität
    - Removed obsolete "url" field of events
    - Changed slug field to provide correct slug for events
    - Added onChipClickHandlers to provide correct navigation to news and events pages with activated tag

--- frontend/src/components/NewsSwiper/NewsSwiperItem.tsx
+++ frontend/src/components/NewsSwiper/NewsSwiperItem.tsx
@@ -29,45 +29,41 @@ export function NewsSwiperItem({
 
   return (
     <>
-    <div
-      className="rounded-lg bg-neutral-200 shadow-sm overflow-hidden h-full"
-    >
-      <a href={slug} className="flex flex-col h-full">        
-        {image !== undefined ? (
-          <div className="news-teaser-image">
-            <Image
-              src={image.src as unknown as IGatsbyImageData}
-              alt={image.alt}
-              className="w-full h-auto aspect-[16/9]"
-            />
+      <div className="rounded-lg bg-neutral-200 shadow-sm overflow-hidden h-full">
+        <a href={slug} className="flex flex-col h-full">
+          {image !== undefined ? (
+            <div className="news-teaser-image">
+              <Image
+                src={image.src as unknown as IGatsbyImageData}
+                alt={image.alt}
+                className="w-full h-auto aspect-[16/9]"
+              />
+            </div>
+          ) : null}
+          <div className="p-8 pb-4">
+            <H3 className="lg:leading-snug mb-4 font-bold">{headline}</H3>
+            <div className="line-clamp-3 mb-8 lg:text-lg">{body}</div>
+            <div className="border-t border-neutral-400 pt-4">
+              <time
+                data-testid="date"
+                dateTime={date.toISOString()}
+                className="block mb-4 font-semibold text-sm"
+              >
+                {formattedDate}
+              </time>
+              <ul className="flex flex-wrap -my-2">
+                {tags.map((tag, index) => {
+                  return (
+                    <li key={`tag-${index}`}>
+                      <Chip {...tag} onClick={onChipClick} />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
           </div>
-        ) : null}        
-        <div className="p-8 pb-4">
-          <H3 className="lg:leading-snug mb-4 font-bold">{headline}</H3>
-          <div className="line-clamp-3 mb-8 lg:text-lg">
-            {body}
-          </div>
-          <div className="border-t border-neutral-400 pt-4">
-            <time
-              data-testid="date"
-              dateTime={date.toISOString()}
-              className="block mb-4 font-semibold text-sm"
-            >
-              {formattedDate}
-            </time>
-            <ul className="flex flex-wrap -my-2">
-              {tags.map((tag, index) => {
-                return (
-                  <li key={`tag-${index}`}>
-                    <Chip {...tag} onClick={() => {}} />
-                  </li>
-                );
-              })}
-            </ul>
-          </div>  
-        </div>                      
-      </a>
-    </div>    
+        </a>
+      </div>
     </>
   );
 }
